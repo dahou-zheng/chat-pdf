89c7afe7a3341d4c59649d14d2fac3269fbdd455
LAPTOP-82OFJAK7\dahou <dahouzheng1999@gmail.com>
2026-01-13 17:51:17 -0800

    Use Open AI's Embedding Model text-embedding-3-small as embedding for text chunks to vectors

--- src/config.py
+++ src/config.py
@@ -6,14 +6,16 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 if not OPENAI_API_KEY:
     raise ValueError("OPENAI_API_KEY not found! Did you forget to set it in your .env file?")
 
+
 BASE_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'data')
 TEST_PDFS_DIR = os.path.join(BASE_DIR, "test_data")
 VECTOR_STORE_DIR = os.path.join(BASE_DIR, "vector_store")
 VECTOR_INDEX_PATH = os.path.join(VECTOR_STORE_DIR, "index.faiss")
 META_PATH = os.path.join(VECTOR_STORE_DIR, "meta.json")
 
-OLLAMA_URL = "http://localhost:11434"
-DEFAULT_MODEL = "mistral:latest"
+EMBEDDING_URL = "https://api.openai.com/v1/embeddings"
+EMBEDDING_MODEL = "text-embedding-3-small"
+DEFAULT_MODEL = "gpt-4o-mini"
 
 
 DEFAULT_TOP_K = 10
---
+++ src/embedding.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from typing import List
+import requests
+from config import OPENAI_API_KEY, EMBEDDING_MODEL, EMBEDDING_URL
+
+
+def embedding(inputs: List[str]) -> List[List[float]]:
+    """Get embeddings from the embedding service"""
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {OPENAI_API_KEY}"
+    }
+    data = {
+        "input": inputs,
+        "model": EMBEDDING_MODEL
+    }
+
+    response = requests.post(EMBEDDING_URL, headers=headers, json=data)
+    outputs = [output['embedding'] for output in response.json()['data']]
+    return outputs
+
+
+if __name__ == '__main__':
+    test_inputs = ["Hello world!", "How are you?"]
+    test_outputs = embedding(test_inputs)
+    test_output = test_outputs[0]
+    print(test_output)
+    print("Dim: ", len(test_output))
